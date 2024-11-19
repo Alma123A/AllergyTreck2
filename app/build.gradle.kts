d75680263fb2408136b3c0f38268cbe6807f6d9e
Alma123A <alma.tisch1@gmail.com>
2024-11-19 10:02:08 +0200

    login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.base)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-    implementation ("com.google.android.gms:playservices-auth:20.7.0")
-}
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    }
+
