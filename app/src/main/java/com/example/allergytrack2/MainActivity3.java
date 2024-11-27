25e68a493c40d7d10c4b032d8e96c1dd8b109849
Alma123A <alma.tisch1@gmail.com>
2024-11-27 15:24:27 +0200

    new activity

---
+++ app/src/main/java/com/example/allergytrack2/MainActivity3.java
@@ -0,0 +1,27 @@
+package com.example.allergytrack2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity3 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        MaterialButton btnNewTrack = findViewById(R.id.btnNewTrack);
+        MaterialButton btnLogin = findViewById(R.id.btnLogin);
+
+        btnNewTrack.setOnClickListener(v -> {
+            // מעבר למסך יצירת מעקב חדש
+            startActivity(new Intent(this, NewTrackActivity.class));
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            // מעבר למסך התחברות
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+    }
+}
